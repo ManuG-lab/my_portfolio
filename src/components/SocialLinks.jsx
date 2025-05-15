62ab8522d5cc23895f0b52078c16bbbf6650121c
Emmanuel Gitau <gmannu2055@gmail.com>
2025-05-15 15:17:58 +0300

    socials added

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,26 +1,26 @@
 import React from "react";
-import { FaGithub, FaLinkedin, FaTwitter, FaEnvelope } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaTwitter, FaEnvelope, FaInstagram } from "react-icons/fa";
 
 const SocialLinks = () => {
   const socials = [
     {
       icon: <FaGithub />,
-      url: "https://github.com/yourusername",
+      url: "https://github.com/ManuG-lab",
       label: "GitHub",
     },
     {
       icon: <FaLinkedin />,
-      url: "https://linkedin.com/in/yourprofile",
+      url: "https://linkedin.com/in/emmanuel-gitau",
       label: "LinkedIn",
     },
     {
-      icon: <FaTwitter />,
-      url: "https://twitter.com/yourhandle",
+      icon: <FaInstagram />,
+      url: "https://www.instagram.com/____justromeo____/",
       label: "Twitter",
     },
     {
       icon: <FaEnvelope />,
-      url: "mailto:youremail@example.com",
+      url: "mailto:gmannu2055@gmail.com",
       label: "Email",
     },
   ];
